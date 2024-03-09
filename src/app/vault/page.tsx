d75259a55ae1624d5a91a7114c0068665beed933
Michelangelo Gelardi <mgelardi@gmail.com>
2024-03-09 15:56:35 +0000

    sql16

--- src/app/vault/page.tsx
+++ src/app/vault/page.tsx
@@ -1,32 +1,41 @@
 "use client";
 import React, { useState , useEffect } from 'react';
 import CommonLayout from "@/layout/CommonLayout";
-import axios from 'axios';
+import mysql from 'mysql';
 
 //DB
-const DataDisplay = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-      axios.get('https://timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com:3360/api/data')
-          .then(response => {
-              setData(response.data);
-          })
-          .catch(error => {
-              console.error('Error fetching data:', error);
-          });
-  }, []);
+const App = () => {
+    const [data, setData] = useState([]);
 
-  return (
-    <CommonLayout mainClass="custom-padding" headerClassName="header-light" sideBarClassName="sidebar-white" loaderName="style2" differentLogo="logo-colore.png">
-    <h1>Data from MySQL Database</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.firstname}>{item.lastname}</li>
-        ))}
-      </ul>
-    </CommonLayout>
-  );
+    useEffect(() => {
+        const connection = mysql.createConnection({
+            host: 'timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com',
+            user: 'admin',
+            password: 'P4p3r1n0!',
+            database: 'tvdb'
+        });
+
+        connection.connect();
+
+        connection.query('SELECT * FROM persons', (error, results, fields) => {
+            if (error) throw error;
+            setData(results);
+        });
+
+        connection.end();
+    }, []);
+
+    return (
+      <CommonLayout mainClass="custom-padding" headerClassName="header-light" sideBarClassName="sidebar-white" loaderName="style2" differentLogo="logo-colore.png">
+      <h1>Data from MySQL Database</h1>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </CommonLayout>
+    );
 };
 
-export default DataDisplay;
+export default App;
