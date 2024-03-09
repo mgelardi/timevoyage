c5591f0942d027ef5be03dd201d9db6f37108e53
Michelangelo Gelardi <mgelardi@gmail.com>
2024-03-09 21:23:57 +0000

    sql555

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,32 @@
 // App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import dynamic from 'next/dynamic';
-
-const ClientOnlyComponent = dynamic(() => import('../components/ClientOnlyComponent'), { ssr: false });
 
 const App = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        axios.get('http://timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com/api/data')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        // Check if we're in the browser environment before making the API call
+        if (typeof window !== 'undefined') {
+            axios.get('http://timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com/api/data')
+                .then(response => {
+                    setData(response.data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     }, []);
 
     return (
-        <ClientOnlyComponent>
-            <div>
-                <h1>Data from MySQL Database</h1>
-                <ul>
-                    {data.map(item => (
-                        <li key={item.id}>{item.name}</li>
-                    ))}
-                </ul>
-            </div>
-        </ClientOnlyComponent>
+        <div>
+            <h1>Data from MySQL Database</h1>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
