574f126d92fd84b0b93df044487297b9c070e108
Michelangelo Gelardi <mgelardi@gmail.com>
2024-03-09 19:28:19 +0000

    mysql1

--- src/app/vault/page.tsx
+++ src/app/vault/page.tsx
@@ -1,47 +1,44 @@
 "use client";
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CommonLayout from "@/layout/CommonLayout";
 const mysql = require('mysql');
 
 //DB
 
 const App = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-      const connection = mysql.createConnection({
-            host: 'https://timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com',
+    useEffect(() => {
+        const connection = mysql.createConnection({
+            host: 'timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com',
             user: 'admin',
             password: 'P4p3r1n0!',
             database: 'tvdb'
         });
 
         connection.connect((error) => {
-          if (error) {
-              console.error('Error connecting to database:', error);
-              return;
-          }
-          console.log('Connected to database');
-      });
+            if (error) {
+                console.error('Error connecting to database:', error);
+                return;
+            }
+            console.log('Connected to database');
 
-        connection.connect();
-
-        connection.query('SELECT * FROM persons', (error, results) => {
-          if (error) throw error;
-          console.log(results);
-          setData(results);
-      });
-
-        connection.end();
+            connection.query('SELECT * FROM persons', (error, results) => {
+                if (error) throw error;
+                console.log(results);
+                setData(results);
+                connection.end(); // Close connection after querying data
+            });
+        });
     }, []);
 
     return (
-      <CommonLayout mainClass="custom-padding" headerClassName="header-light" sideBarClassName="sidebar-white" loaderName="style2" differentLogo="logo-colore.png">
-      <h1>Data from MySQL Database</h1>
-        <ul>
-        
-        </ul>
-      </CommonLayout>
+        <CommonLayout mainClass="custom-padding" headerClassName="header-light" sideBarClassName="sidebar-white" loaderName="style2" differentLogo="logo-colore.png">
+            <h1>Data from MySQL Database</h1>
+            <ul>
+               
+            </ul>
+        </CommonLayout>
     );
 };
 
