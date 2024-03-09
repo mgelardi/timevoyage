1df2e87c0ec239c90852c07e8765fa5ea368cc72
Michelangelo Gelardi <mgelardi@gmail.com>
2024-03-09 20:16:47 +0000

    sql222

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,8 @@
 // App.js
+// @ts-nocheck
+// @ts-ignore
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-// Mark the component as a client component
-// This ensures that components using React hooks work correctly in a server-side rendered context
-// See https://nextjs.org/docs/getting-started/react-essentials#client-components
-const useClient = () => {};
-useClient;
 
 const App = () => {
     const [data, setData] = useState([]);
