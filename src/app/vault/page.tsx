1f9d477e3e51ab7f830d4cb1f1e5a71cf7a70ceb
Michelangelo Gelardi <mgelardi@gmail.com>
2024-03-09 22:00:53 +0000

    sql1234

--- src/app/vault/page.tsx
+++ src/app/vault/page.tsx
@@ -1,14 +1,45 @@
-// page.stx
-import React, { useState , useEffect } from 'react'; // Import useState and useEffect hooks
-import App from '../App.js'; // Assuming App.js is in the same directory
+"use client";
+import React, { useState, useEffect } from 'react';
+import CommonLayout from "@/layout/CommonLayout";
+const mysql = require('mysql');
 
-const Page = () => {
+//DB
+
+const App = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const connection = mysql.createConnection({
+            host: 'timevoyagedb.crm0qiaemqmq.eu-west-1.rds.amazonaws.com',
+            user: 'admin',
+            password: 'P4p3r1n0!',
+            database: 'tvdb'
+        });
+
+        connection.connect((error) => {
+            if (error) {
+                console.error('Error connecting to database:', error);
+                return;
+            }
+            console.log('Connected to database');
+
+            connection.query('SELECT * FROM persons', (error, results) => {
+                if (error) throw error;
+                console.log(results);
+                setData(results);
+                connection.end(); // Close connection after querying data
+            });
+        });
+    }, []);
+
     return (
-        <div>
-            <h1>Your Page Title</h1>
-            <App /> {/* Render the App component */}
-        </div>
+        <CommonLayout mainClass="custom-padding" headerClassName="header-light" sideBarClassName="sidebar-white" loaderName="style2" differentLogo="logo-colore.png">
+            <h1>Data from MySQL Database</h1>
+            <ul>
+               
+            </ul>
+        </CommonLayout>
     );
 };
 
-export default Page;
+export default App;
